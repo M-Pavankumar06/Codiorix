086f4d63ea817e21a0e1a158fae2962286a44e2c
M-Pavankumar06 <24bcad29@kristujayanti.com>
2025-10-17 22:53:26 +0530

    ready

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,14 +4,23 @@ import dotenv from 'dotenv';
 dotenv.config();
 import connectDb from './Configuration/db.js'
 import contactRoutes from './routes/ContactRoutes.js';
+import path from 'path';
 
 const app = express();
-const PORT = process.env.PORT || " http://localhost:5000/" ;
+const PORT = process.env.PORT || " http://localhost:5000/";
+const __dirname = path.resolve();
 
 app.use(cors());
 app.use(express.json());
 
 app.use('/api/contacts', contactRoutes);
+if (process.env.NODE_ENV === 'production') {
+   app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+   app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+   })
+}
 
 connectDb().then(() => {
    app.listen(PORT, () => console.log(`Server running on ${PORT}`));
