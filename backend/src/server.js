9cfe9f8a794cf459472584647c6789f0a46146f2
M-Pavankumar06 <24bcad29@kristujayanti.com>
2025-10-18 14:24:30 +0530

    done

--- backend/src/server.js
+++ backend/src/server.js
@@ -10,22 +10,14 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
-if (process.env.NODE_ENV !== 'production') {
-   app.use(cors({
-      origin: "http://localhost:5173"
-   }));
-}
-
+app.use(cors());
 app.use(express.json());
 
 app.use('/api/contacts', contactRoutes);
-if (process.env.NODE_ENV === 'production') {
-   app.use(express.static(path.join(__dirname, "../frontend/dist")));
-   app.get("*", (req, res) => {
-      res.sendFile(path.join(__dirname, "../frontend","dist", "index.html"));
-   });
-}
-
+app.use(express.static(path.join(__dirname, "./frontend/dist")));
+app.get('*',(req,res)=>{
+   res.sendFile(path.resolve(__dirname, "./frontend","dist","index.html"));
+});
 
 connectDb().then(() => {
    app.listen(PORT, () => console.log(`Server running on ${PORT}`));
