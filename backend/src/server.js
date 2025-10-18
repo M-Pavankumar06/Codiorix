a76bc1894fd3bf8052e87e888456a9067ad8d8da
M-Pavankumar06 <24bcad29@kristujayanti.com>
2025-10-18 15:01:35 +0530

    done2

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,18 +5,20 @@ dotenv.config();
 import connectDb from './Configuration/db.js'
 import contactRoutes from './routes/ContactRoutes.js';
 import path from 'path';
+import { fileURLToPath } from 'url';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const __dirname = path.resolve();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(cors());
 app.use(express.json());
 
 app.use('/api/contacts', contactRoutes);
-app.use(express.static(path.join(__dirname, "./frontend/dist")));
+app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 app.all('/*',(req,res)=>{
-   res.sendFile(path.resolve(__dirname, "./frontend","dist","index.html"));
+   res.sendFile(path.resolve(__dirname, "../../frontend","dist","index.html"));
 });
 
 connectDb().then(() => {
