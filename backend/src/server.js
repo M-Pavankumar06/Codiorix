6c8d9e274930911d8cbf4e5a598ad746537d7d63
M-Pavankumar06 <24bcad29@kristujayanti.com>
2025-10-18 16:00:10 +0530

    package u

--- backend/src/server.js
+++ backend/src/server.js
@@ -12,14 +12,18 @@ const PORT = process.env.PORT || 5000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use(cors());
+if(process.env.NODE_ENV !== 'production'){
+   app.use(cors());
+}
+
 app.use(express.json());
 
 app.use('/api/contacts', contactRoutes);
+if(process.env.NODE_ENV === 'production'){
 app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 app.get('*',(req,res)=>{
    res.sendFile(path.resolve(__dirname, "../../frontend","dist","index.html"));
-});
+});}
 
 connectDb().then(() => {
    app.listen(PORT, () => console.log(`Server running on ${PORT}`));
