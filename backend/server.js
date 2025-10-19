337dba29f5a471dbfd4ce1e722882c5be57503aa
M-Pavankumar06 <24bcad29@kristujayanti.com>
2025-10-19 09:37:25 +0530

    link u

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 5000 ;
 const __dirname = path.resolve();
 const corsOptions = {
-   orgin: "http://localhost:5173",
+   orgin: "https://codiorixweb.onrender.com/",
    Credentials: true,
 }
 app.use(cors(corsOptions));
